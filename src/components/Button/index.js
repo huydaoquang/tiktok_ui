6efe6874afd1b2012de12af82a7cad9e3a922af5
huy dao <you@example.com>
2023-01-22 12:53:50 +0700

    update header#3

---
+++ src/components/Button/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './Button.module.scss';
+import { Link } from 'react-router-dom';
+const cx = classNames.bind(styles);
+
+const Button = ({
+  to,
+  href,
+  primary = false,
+  outline = false,
+  text = false,
+  disabled = false,
+  rounded = false,
+  small = false,
+  large = false,
+  children,
+  className,
+  lefIcon,
+  rightIcon,
+  onClick,
+  ...passProps
+}) => {
+  let Comp = 'button';
+  const props = {
+    onClick,
+    ...passProps,
+  };
+
+  // Remove event listener when btn is disabled
+  if (disabled) {
+    Object.keys(props).forEach((key) => {
+      if (key.startsWith('on') && typeof props[key] === 'function') {
+        delete props[key];
+      }
+    });
+  }
+  if (to) {
+    props.to = to;
+    Comp = Link;
+  } else if (href) {
+    props.href = href;
+    Comp = 'a';
+  }
+
+  const classes = cx('wrapper', {
+    [className]: className,
+    primary,
+    outline,
+    text,
+    disabled,
+    rounded,
+    small,
+    large,
+  });
+
+  return (
+    <Comp className={classes} {...props}>
+      {lefIcon && <span className={cx('icon')}>{lefIcon}</span>}
+      <span className={cx('title')}>{children}</span>
+      {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
+    </Comp>
+  );
+};
+
+export default Button;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -5,6 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react/headless';
 
+import Button from '~/components/Button';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
@@ -51,7 +52,10 @@ const Header = () => {
             </button>
           </div>
         </Tippy>
-        <div className={cx('action')}></div>
+        <div className={cx('action')}>
+          <Button text>Upload</Button>
+          <Button primary>Log in</Button>
+        </div>
       </div>
     </header>
   );
